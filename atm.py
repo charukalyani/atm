4436f4bf0126287cc34fcc6e638f37a4863c0ca6
Vineet Kumar <62938573+vin8bit@users.noreply.github.com>
2022-10-01 00:18:16 +0530

    Change the name

--- atm.py
+++ atm.py
@@ -10,7 +10,7 @@ amounts = [1000, 2000, 3000]
 count = 0
 # while loop checks existance of the enterd username
 while True:
-	user = input('\nENTER USER NAME: ')
+	user = input('\nENTER USER Name: ')
 	user = user.lower()
 	if user in users:
 		if user == users[0]:
